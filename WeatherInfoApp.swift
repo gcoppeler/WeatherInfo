e137982a3823a1a3047ac83bb7d899f43cf860cb
Gary Coppeler <gcoppeler@outlook.com>
2023-04-26 12:53:55 -0400

    Initial Xcode project

---
+++ WeatherInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherInfoApp.swift
+//  WeatherInfo
+//
+//  Created by Gary Coppeler on 4/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
